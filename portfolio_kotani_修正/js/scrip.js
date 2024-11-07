2765b85e39678f04c36f61be13672226e243b82f
coconoa1802 <cocomil12051924@gmail.com>
2024-11-07 11:55:32 +0900

    portfolio

--- portfolio_kotani_修正/js/scrip.js
+++ portfolio_kotani_修正/js/scrip.js
@@ -158,17 +158,16 @@ var myDate = String(myD.getDate());//日
 var splash_text = $.cookie('accessdate'); //キーが入っていれば年月日を取得
 
 if (splash_text != myYear + myMonth + myDate) {//cookieデータとアクセスした日付を比較↓
-  setTimeout(function () {
+  $("#splash").css("display", "block");
   stroke.play();//SVGアニメーションの実行
-    $("#splash").delay(2500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェイドアウト
-	  $("#splash_logo").delay(2500).fadeOut('slow');//ロゴを1.5秒（1500ms）待機してからフェイドアウト
+  $("#splash").delay(2500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェイドアウト
+	$("#splash_logo").delay(2500).fadeOut('slow');//ロゴを1.5秒（1500ms）待機してからフェイドアウト
   
   var myD = new Date();
   var myYear = String(myD.getFullYear());
   var myMonth = String(myD.getMonth() + 1);
   var myDate = String(myD.getDate());
   $.cookie('accessdate', myYear + myMonth + myDate); //accessdateキーで年月日を記録
-  }, 1000);
 }else {
     $("#splash").css("display", "none");//同日2回目のアクセスでローディング画面非表示
 }  
